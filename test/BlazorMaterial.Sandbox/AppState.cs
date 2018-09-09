91553230c62e0de7f9893216e6f649307a12f63a
Eric Montgomery <ericm@techpyramid.ws>
2018-09-09 02:11:00 -0600

    Adding new sandbox to fix execution flaws

---
+++ test/BlazorMaterial.Sandbox/AppState.cs
@@ -0,0 +1,9 @@
+namespace BlazorMaterial.Sandbox
+{
+    public class AppState
+    {
+        public string ToolbarIcon { get; set; }
+        public string Title { get; set; }
+        public bool ShowTopAppBar { get; set; } = true;
+    }
+}
